a911ac16bfaa28f0cc411bd2014e1ef45e99ac8b
asadbek <asadbekqulboyev@gmail.com>
2025-02-11 21:35:32 +0500

    desctop 50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,20 @@ $(document).ready(function () {
     e.preventDefault();
     $(this).next('.submenu').slideToggle();
     });
+    var swiper = new Swiper('.testimonial_slider', {
+        loop: false,
+        autoHeight: true,
+    });
+
+    $('.avatar').click(function() {
+        var index = $(this).data('index');
+        swiper.slideTo(index);
+        $('.avatar').removeClass('active');
+        $(this).addClass('active');
+    });
+    swiper.on('slideChange', function () {
+        var activeIndex = swiper.activeIndex;
+        $('.avatar').removeClass('active');
+        $('.avatar[data-index="' + activeIndex + '"]').addClass('active');
+    });
 })
